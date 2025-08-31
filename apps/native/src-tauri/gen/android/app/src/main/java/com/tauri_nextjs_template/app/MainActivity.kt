ec04ee706b02bbc815e4d45d2e48fffee57b8122
dest <destrochloridium@gmail.com>
2025-08-31 18:17:34 +0300

    feat(android): add Android build configuration and resources

---
+++ apps/native/src-tauri/gen/android/app/src/main/java/com/tauri_nextjs_template/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tauri_nextjs_template.app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
